bcc6e250cfa3fb9f978ed7a4e5ebbeadbeda4d5b
HarimHussain <156240520+HarimHussain@users.noreply.github.com>
2024-01-18 21:56:01 +0500

    Update Harim_Mid Term.py

--- Harim_Mid Term.py
+++ Harim_Mid Term.py
@@ -3,15 +3,18 @@
 #Q1 - Write a Python program to do arithmetical operations addition and division.
 
 
-
 Num1=43
 Num2=43
 Sum=Num1+Num2
-print('Num1+Num2')
+print(Sum)
+
+
 Num1=33
 Num2=23
 Subtract=Num1-Num2
-print('Num1-Num2')
-First_Number=(int('Enter_first_number'))
-Second_Number=(int('Enter_Second_Number'))
-print('sumFirst_number,''add','Second_Number')
+print(Subtract)
+
+
+First_Number=(int(input('Enter_first_number'))
+Second_Number=(intinput('Enter_Second_Number'))
+print(First_Number/Second_Number)
