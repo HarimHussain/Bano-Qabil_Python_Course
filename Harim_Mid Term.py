8f299e9d6858f8e1faa6c09a87962bc71a9d54c9
HarimHussain <156240520+HarimHussain@users.noreply.github.com>
2024-01-18 21:49:13 +0500

    Add files via upload

---
+++ Harim_Mid Term.py
@@ -0,0 +1,17 @@
+# Muhammad_Harim
+# Mid_Term_Exam
+#Q1 - Write a Python program to do arithmetical operations addition and division.
+
+
+
+Num1=43
+Num2=43
+Sum=Num1+Num2
+print('Num1+Num2')
+Num1=33
+Num2=23
+Subtract=Num1-Num2
+print('Num1-Num2')
+First_Number=(int('Enter_first_number'))
+Second_Number=(int('Enter_Second_Number'))
+print('sumFirst_number,''add','Second_Number')
